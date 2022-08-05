2035cf72c909c2fdf629f0524afb08a0c15ac39f
Daniel-Runge <54961055+Daniel-Runge@users.noreply.github.com>
2022-08-05 19:03:15 +0200

    Fix PATH export.

--- setup-P5.sh
+++ setup-P5.sh
@@ -3,13 +3,12 @@
 {
 LIST_OF_PACKAGES="build-essential unzip bat python3-pygments python-is-python3 python3 python3-pip ipython3 postgresql postgresql-contrib"
 BAT_ALIAS="alias cat='batcat --paging=never'"
-LOCAL_BIN_EXPORT='export PATH=$HOME/.local/bin:$PATH'
 
 echo This script installs the recommended packages for the 5th semester at AAU: C compiler, python basics, postgresql, and LaTeX, and downloads the aipython libraries.
 
 printf "\\n${BAT_ALIAS}\\n" >> "${HOME}/.bashrc"
-printf "\\n${LOCAL_BIN_EXPORT}\\n" >> "${HOME}/.bashrc"
-source ${HOME}/.bashrc
+source .bashrc
+export PATH=$HOME/.local/bin:$PATH
 
 curl -fsSLO https://artint.info/AIPython/aipython.zip &
 
