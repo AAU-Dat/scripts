edb5538a0ba557032edcaedadc795395b464608b
Daniel-Runge <rungedaniel@hotmail.com>
2022-08-05 22:33:18 +0200

    Add script to install VS Code extensions

---
+++ setup-vscode.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+{
+declare -a StringArray=("ms-vscode.cpptools-extension-pack" "ms-python.python" "cweijan.vscode-database-client2" "eamodio.gitlens"
+"ms-toolsai.jupyter" "james-yu.latex-workshop" "yzhang.markdown-all-in-one" "bierner.markdown-mermaid" "pkief.material-icon-theme")
+
+for val in "${StringArray[@]}"; do
+    code --install-extension $val --force
+  echo $val
+done
+
+}
