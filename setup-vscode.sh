d65760c7485c712e5b8347f568aa5072bb64df88
Daniel-Runge <rungedaniel@hotmail.com>
2022-08-05 22:39:14 +0200

    Extensions installed as background processes.

--- setup-vscode.sh
+++ setup-vscode.sh
@@ -5,8 +5,10 @@ declare -a StringArray=("ms-vscode.cpptools-extension-pack" "ms-python.python" "
 "ms-toolsai.jupyter" "james-yu.latex-workshop" "yzhang.markdown-all-in-one" "bierner.markdown-mermaid" "pkief.material-icon-theme")
 
 for val in "${StringArray[@]}"; do
-    code --install-extension $val --force
+    code --install-extension $val --force &
   echo $val
 done
+
+wait
 
 }
