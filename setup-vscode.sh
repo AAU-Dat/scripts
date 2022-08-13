4b28d990c627b5095ddead56c77b00cfdaf1b76a
Daniel-Runge <54961055+Daniel-Runge@users.noreply.github.com>
2022-08-13 14:47:20 +0200

    Add code call before installing extensions

--- setup-vscode.sh
+++ setup-vscode.sh
@@ -4,6 +4,8 @@
 declare -a StringArray=("ms-vscode.cpptools-extension-pack" "ms-python.python" "cweijan.vscode-database-client2" "eamodio.gitlens"
 "ms-toolsai.jupyter" "james-yu.latex-workshop" "yzhang.markdown-all-in-one" "bierner.markdown-mermaid" "pkief.material-icon-theme")
 
+code .
+
 for val in "${StringArray[@]}"; do
     code --install-extension $val --force &
   echo $val
