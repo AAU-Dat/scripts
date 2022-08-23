85109f3f1058eeae1d2b36772cf46888144e6872
Daniel-Runge <54961055+Daniel-Runge@users.noreply.github.com>
2022-08-23 07:16:28 +0200

    Add python testresources. Add pip install pandas

--- setup-P5.sh
+++ setup-P5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 {
-LIST_OF_PACKAGES="build-essential gdb unzip bat python3-pip python-is-python3 postgresql postgresql-contrib texlive-full"
+LIST_OF_PACKAGES="build-essential gdb unzip bat python3-pip python3-testresources python-is-python3 postgresql postgresql-contrib texlive-full"
 BAT_ALIAS="alias cat='batcat --paging=never'"
 
 echo This script installs the recommended packages for the 5th semester at AAU: C compiler, python basics, postgresql, and LaTeX, and downloads the aipython libraries.
@@ -15,7 +15,7 @@ curl -fsSLO https://artint.info/AIPython/aipython.zip &
 sudo apt update && sudo apt upgrade -y && sudo apt install ${LIST_OF_PACKAGES} -y
 sudo apt autoremove -y
 pip install -U pip
-pip install -U matplotlib jupyterlab
+pip install -U matplotlib jupyterlab pandas
 
 wait
 
