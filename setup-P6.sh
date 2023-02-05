4ab6d56dcf2e9d16c120d8eae57e8afc91e670dc
Daniel-Runge <rungedaniel@hotmail.com>
2023-02-05 11:14:14 +0100

    Formatting

---
+++ setup-P6.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+{
+    LIST_OF_PACKAGES="build-essential gdb unzip bat python3-pip python3-testresources python-is-python3 texlive-full"
+    BAT_ALIAS="alias cat='batcat --paging=never'"
+    
+    echo "This script installs the recommended packages for the 6th semester at AAU: C compiler, python basics, and LaTeX."
+    
+    printf "\\n${BAT_ALIAS}\\n" >> "${HOME}/.bashrc"
+    source .bashrc
+    
+    export PATH=$HOME/.local/bin:$PATH
+    sudo apt update && sudo apt upgrade -y && sudo apt install "${LIST_OF_PACKAGES[@]}" -y
+    sudo apt autoremove -y
+    pip install -U pip
+    pip install -U matplotlib jupyterlab pandas Jinja2 scikit-learn
+}
